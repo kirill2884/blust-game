43d873180c713987502bcca818b1dff8faf8d275
k.chistin <K.Chistin@zelenski.ru>
2025-06-24 11:57:34 +0300

    attemp 3

--- docs/src/settings.29715.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/MobileScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",main:"93b54"}};
